529aa4a1ab1af73a682fc2343b26f3cb6b56c2b3
Olga Biro <ola@biro.family>
2025-05-11 16:45:58 +0200

    Changed to a more modern @if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from './user/user.component';
 import {DUMMY_USERS} from './dummy-users';
-import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    UserComponent,
-    NgForOf
+    UserComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
