70174f0d02a0b465d266fd7ced4be95c3f454a29
Olga Biro <ola@biro.family>
2025-05-16 23:26:18 +0200

    Implemented task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,48 @@
 import {Component, Input} from '@angular/core';
+import {Task, TaskComponent} from './task/task.component';
+
+
+const dummyTasks: Task[] = [
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+]
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [
+    TaskComponent
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() userName?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) userName!: string;
+  tasks: Task[] = dummyTasks
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
